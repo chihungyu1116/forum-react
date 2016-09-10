7f32067e886d5b2b6a609d4ef70afbcbe503b3c5
chihungyu1116 <chihungyu1116@gmail.com>
2016-09-09 17:50:47 -0700

    working on modifying the boiler plate

--- src/config.js
+++ src/config.js
@@ -15,21 +15,21 @@ module.exports = Object.assign({
   apiHost: process.env.APIHOST || 'localhost',
   apiPort: process.env.APIPORT,
   app: {
-    title: 'React Redux Example',
-    description: 'All the modern best practices in one example.',
+    title: 'Leeft',
+    description: '',
     head: {
-      titleTemplate: 'React Redux Example: %s',
+      titleTemplate: 'Portal To Top Tech Companies: %s',
       meta: [
-        {name: 'description', content: 'All the modern best practices in one example.'},
+        {name: 'description', content: 'Portal To Top Tech Companies.'},
         {charset: 'utf-8'},
-        {property: 'og:site_name', content: 'React Redux Example'},
+        {property: 'og:site_name', content: 'Portal To Top Tech Companies'},
         {property: 'og:image', content: 'https://react-redux.herokuapp.com/logo.jpg'},
         {property: 'og:locale', content: 'en_US'},
-        {property: 'og:title', content: 'React Redux Example'},
-        {property: 'og:description', content: 'All the modern best practices in one example.'},
+        {property: 'og:title', content: 'Portal To Top Tech Companies'},
+        {property: 'og:description', content: 'Portal To Top Tech Companies'},
         {property: 'og:card', content: 'summary'},
-        {property: 'og:site', content: '@erikras'},
-        {property: 'og:creator', content: '@erikras'},
+        {property: 'og:site', content: '@cyu0'},
+        {property: 'og:creator', content: '@cyu0'},
         {property: 'og:image:width', content: '200'},
         {property: 'og:image:height', content: '200'}
       ]
--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,15 @@ import {IndexRoute, Route} from 'react-router';
 import { isLoaded as isAuthLoaded, load as loadAuth } from 'redux/modules/auth';
 import {
     App,
-    Chat,
+    // Chat,
     Home,
-    Widgets,
-    About,
-    Login,
+    Work,
+    // Widgets,
+    // About,
+    // Login,
     LoginSuccess,
-    Survey,
-    NotFound,
+    // Survey,
+    // NotFound,
   } from 'containers';
 
 export default (store) => {
@@ -31,28 +32,39 @@ export default (store) => {
     }
   };
 
-  /**
-   * Please keep routes in alphabetical order
-   */
   return (
     <Route path="/" component={App}>
-      { /* Home (main) route */ }
       <IndexRoute component={Home}/>
-
-      { /* Routes requiring login */ }
       <Route onEnter={requireLogin}>
-        <Route path="chat" component={Chat}/>
         <Route path="loginSuccess" component={LoginSuccess}/>
       </Route>
 
-      { /* Routes */ }
-      <Route path="about" component={About}/>
-      <Route path="login" component={Login}/>
-      <Route path="survey" component={Survey}/>
-      <Route path="widgets" component={Widgets}/>
-
-      { /* Catch all route */ }
-      <Route path="*" component={NotFound} status={404} />
+      <Route path="work" component={Work}/>
     </Route>
   );
+
+  /**
+   * Please keep routes in alphabetical order
+   */
+  // return (
+  //   <Route path="/" component={App}>
+  //     { /* Home (main) route */ }
+  //     <IndexRoute component={Home}/>
+
+  //     { /* Routes requiring login */ }
+  //     <Route onEnter={requireLogin}>
+  //       <Route path="chat" component={Chat}/>
+  //       <Route path="loginSuccess" component={LoginSuccess}/>
+  //     </Route>
+
+  //     { /* Routes */ }
+  //     <Route path="about" component={About}/>
+  //     <Route path="login" component={Login}/>
+  //     <Route path="survey" component={Survey}/>
+  //     <Route path="widgets" component={Widgets}/>
+
+  //     { /* Catch all route */ }
+  //     <Route path="*" component={NotFound} status={404} />
+  //   </Route>
+  // );
 };
